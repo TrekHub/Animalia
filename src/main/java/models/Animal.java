1586dffafdd16b6aaa5f90b7dd719722c2612381
TrekHub <teddyomondi001@gmail.com>
2022-02-09 11:44:02 +0300

    created the animal object

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,29 @@
 package models;
 
 public class Animal {
+
+    private String name;
+    private String age;
+
+    public Animal(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
